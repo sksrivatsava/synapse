073ac1ec7651deaf98821c03ad317a1c716c45dd
sksrivatsava <sksrivatsava@gmail.com>
2021-12-13 19:51:08 +0530

    fc2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_core_example/firebase_config.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
